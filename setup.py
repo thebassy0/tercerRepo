7730e276e8165959e50e68d4a30f00425ec0095e
steven <stevenr_28@hotmail.com>
2026-06-08 00:17:20 -0500

    release

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                                 # Nombre del paquete
+    version="0.1.0",                                   # Versión inicial
+    packages=find_packages(),                          # Paquetes a incluir
+    description="Un paquete pip simple de saludo",     # Breve descripción
+    author="Steven Rengifo",                           # Tu nombre
+    author_email="stevenr_28@hotmail.com",             # Tu correo electrónico
+    url="https://github.com/thebassy0/tercerRepo",     # URL del proyecto
+)
